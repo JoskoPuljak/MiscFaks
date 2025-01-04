2a2246b47b882fa74839f109c4ac9f5f0489b4ee
JoškoPuljak <jpuljak@pmfst.hr>
2025-01-04 18:51:32 +0000

    verzija koja radi

--- testmodel.py
+++ testmodel.py
@@ -1,4 +1,4 @@
 import dinamickimodelbicikla as din
-Formula=din.Dinamički_bicikl(100,0.1,0.1,2,1,0.5,10,0.1,0.1,13)
+Formula=din.Dinamički_bicikl(270,1.4,1.4,1.125,1.375,100,10,0.1,0.1,13)
 Formula.test(10,0.01)
 Formula.oversteertest()
